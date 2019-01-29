4c295994e0f1ffd1d91dba1838482e49c6b388b9
nico <nicolas.p.suarez@gmail.com>
2019-01-29 06:38:42 -0300

    Finish API endpoint

--- api/search.py
+++ api/search.py
@@ -1,6 +1,9 @@
-from createapp import sc_scraper
+from createapp import db, sc_scraper
+from datetime import datetime
 from flask import request
 from flask_restful import Resource
+from models.search import Search as SearchModel
+from scscraper import SCScraperException
 
 
 class Search(Resource):
@@ -19,4 +22,58 @@ class Search(Resource):
         # identity information will be searched in Social Catfish instead of
         # doing it in the app DB.
         rescrap = request.path.endswith('/rescrap')
-        return sc_scraper.search(param)
+        search = None
+        if not rescrap:
+            search = \
+                SearchModel.query.filter(SearchModel.param == param).first()
+        if rescrap or not search:
+            # Rescrap was specified or the record was not found in the app DB.
+            # Get the identity information from Social Catfish.
+            try:
+                identity = sc_scraper.search(param)
+                if identity:
+                    # Save the identity in the app DB.
+                    search = self._save_search(param, identity)
+                else:
+                    # The identity was not found.
+                    response = {
+                        'success': False,
+                        'error': 'The indentity was not found.'
+                    }
+            except SCScraperException as e:
+                # There was an error while getting the information.
+                response = {
+                    'success': False,
+                    'error': str(e)
+                }
+        # If the search was completed build a successful response.
+        if search:
+            response = {
+                'success': True,
+                'create_datetime': \
+                    search.create_datetime.strftime("%Y-%m-%d %H:%M:%S"),
+                'identity': search.identity
+            }
+        # Send request response.
+        return response
+
+    def _save_search(self, param: str, identity: dict) -> SearchModel:
+        """
+        Save the identity in the app DB.
+
+        :param param str: Search param.
+        :param dict identity: identity information.
+        """
+        # Check the DB to know if the record was already saved.
+        search = SearchModel.query.filter(SearchModel.param == param).first()
+        if not search:
+            # Create a new record.
+            search = SearchModel()
+        # Save the information.
+        search.param = param
+        search.create_datetime = datetime.utcnow()
+        search.identity = identity
+        db.session.add(search)
+        db.session.commit()
+        # Return the search model.
+        return search
---
+++ models/search.py
@@ -0,0 +1,13 @@
+from createapp import db
+from sqlalchemy_utils.types.json import JSONType
+
+
+class Search(db.Model):
+    """
+    Search model.
+    """
+
+    # Columns.
+    param = db.Column('param', db.String(), primary_key=True)
+    create_datetime = db.Column('create_datetime', db.DateTime(), nullable=False)
+    identity = db.Column('identity', JSONType, nullable=False)
